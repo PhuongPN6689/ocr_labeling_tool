715acb3f6a96a9ecada03a5101c162e5b32d7a3d
Phạm Nguyên Phương <phamnguyenphuong@hotmail.com>
2024-08-06 21:41:10 +0700

    add server setting config gui

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,6 @@
 import copy
 import json
+import socket
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 from PIL import Image, ImageTk
@@ -7,10 +8,98 @@ import os
 
 from src.ocr import load_ocr_model
 
+# import requests
+# import asyncio
+# import websockets
+
+
+class SettingsDialog(tk.Toplevel):
+    def __init__(self, parent, is_open_server, is_connect_to_server, port, base_url):
+        super().__init__(parent)
+        self.is_open_server = is_open_server
+        self.is_connect_to_server = is_connect_to_server
+        self.port = port
+        self.base_url = base_url
+
+        self.title("Settings")
+        self.geometry("700x300")
+
+        # Create a frame to hold the settings in two rows
+        frame = tk.Frame(self)
+        frame.pack(pady=10, padx=10, fill="both", expand=True)
+
+        # Third row
+        row0 = tk.Frame(frame)
+        row0.pack(fill="x", pady=5)
+
+        # Now IP address
+        tk.Label(row0, text="IP Address:").pack(side="left", padx=5)
+        self.ip_address_entry = tk.Entry(row0, width=20)
+        self.ip_address_entry.pack(side="left", padx=5)
+
+        # First row
+        row1 = tk.Frame(frame)
+        row1.pack(fill="x", pady=5)
+
+        tk.Label(row1, text="Open Server Connection:").pack(side="left", padx=5)
+        self.open_server = tk.BooleanVar()
+        self.open_server.set(self.is_open_server)
+        self.server_connection_check = tk.Checkbutton(row1, variable=self.open_server, command=self.open_server_click)
+        self.server_connection_check.pack(side="left", padx=5)
+
+        tk.Label(row1, text="Port:").pack(side="left", padx=5)
+        self.port_entry = tk.Entry(row1, width=4)
+        self.port_entry.insert(0, str(self.port))
+        self.port_entry.pack(side="left", padx=5)
+
+        # Second row
+        row2 = tk.Frame(frame)
+        row2.pack(fill="x", pady=5)
+
+        tk.Label(row2, text="Connect to Another Server:").pack(side="left", padx=5)
+        self.connect_to_server = tk.BooleanVar()
+        self.connect_to_server.set(self.is_connect_to_server)
+        self.another_server_connection_check = tk.Checkbutton(row2, variable=self.connect_to_server, command=self.connect_to_server_click)
+        self.another_server_connection_check.pack(side="left", padx=5)
+
+        tk.Label(row2, text="Base URL of Another Server:").pack(side="left", padx=5)
+        self.base_url_entry = tk.Entry(row2, width=50)
+        self.base_url_entry.insert(0, self.base_url)
+        self.base_url_entry.pack(side="left", padx=5)
+
+        save_button = tk.Button(self, text="Save", command=self.save_settings)
+        save_button.pack(pady=20)
+
+        # get local IP address
+        my_ip = socket.gethostbyname(socket.gethostname())
+        self.ip_address_entry.insert(0, my_ip)
+
+    def open_server_click(self):
+        if self.open_server.get() and self.connect_to_server.get():
+            self.open_server.set(True)
+            self.connect_to_server.set(False)
+
+    def connect_to_server_click(self):
+        if self.open_server.get() and self.connect_to_server.get():
+            self.open_server.set(False)
+            self.connect_to_server.set(True)
+
+    def save_settings(self):
+        self.is_connect_to_server = self.connect_to_server.get()
+        self.is_open_server = self.open_server.get()
+        self.port = int(self.port_entry.get())
+        self.base_url = self.base_url_entry.get()
+        self.destroy()
+
 
 class OCRLabelingTool(tk.Tk):
     def __init__(self):
         super().__init__()
+
+        self.is_open_server = False
+        self.is_connect_to_server = False
+        self.port = 8000
+        self.base_url = "http://192.168.6.9:8000"
 
         # Thiết lập cửa sổ chính
         self.title("OCR Labeling Tool")
@@ -39,19 +128,40 @@ class OCRLabelingTool(tk.Tk):
         self.bind("<Configure>", self.on_resize)
         self.resize_after_id = None
 
+    #     self.server_url = "http://localhost:8000"
+
+    # def open_image_folder_click(self):
+    #     folder_path = filedialog.askdirectory()
+    #     if folder_path:
+    #         self.image_folder = folder_path
+    #         response = requests.post(f"{self.server_url}/load_images", json={"image_folder": folder_path})
+    #         self.image_list = response.json().get("images", [])
+    #         self.load_images_click()
+    #         self.display_image_click()
+    #
+    # def save_label_click(self):
+    #     self.text_entry.focus_set()
+    #     label = self.text_entry.get()
+    #     requests.post(f"{self.server_url}/save_label", json={"image_filename": self.image_filename, "label": label})
+    #     self.old_label_value = label
+    #
+    # def load_label_file(self, image_filename):
+    #     response = requests.get(f"{self.server_url}/get_label", params={"image_filename": image_filename})
+    #     return response.json().get("label", "")
+
     def create_widgets(self):
         # Cột 1: Khu vực bên trái
         left_frame = tk.Frame(self)
         left_frame.pack(side="left", fill="y", padx=10, pady=5)
 
-        open_image_folder_button = tk.Button(left_frame, text="Open Image Folder", compound=tk.TOP, command=self.open_image_folder_click)
-        open_image_folder_button.pack(pady=5, fill="x")
+        self.open_image_folder_button = tk.Button(left_frame, text="Open Image Folder", compound=tk.TOP, command=self.open_image_folder_click)
+        self.open_image_folder_button.pack(pady=5, fill="x")
 
-        open_label_folder_button = tk.Button(left_frame, text="Open Label Folder", compound=tk.TOP, command=self.open_label_folder_click)
-        open_label_folder_button.pack(pady=5, fill="x")
+        self.open_label_folder_button = tk.Button(left_frame, text="Open Label Folder", compound=tk.TOP, command=self.open_label_folder_click)
+        self.open_label_folder_button.pack(pady=5, fill="x")
 
-        open_label_folder_button = tk.Button(left_frame, text="Open Recycle Bin Folder", compound=tk.TOP, command=self.open_recycle_bin_click)
-        open_label_folder_button.pack(pady=5, fill="x")
+        self.open_recycle_bin_folder_button = tk.Button(left_frame, text="Open Recycle Bin Folder", compound=tk.TOP, command=self.open_recycle_bin_click)
+        self.open_recycle_bin_folder_button.pack(pady=5, fill="x")
 
         reload_button = tk.Button(left_frame, text="Reload Files", compound=tk.TOP, command=self.load_images_click)
         reload_button.pack(pady=5, fill="x")
@@ -66,10 +176,13 @@ class OCRLabelingTool(tk.Tk):
         self.model_dropdown.set(options[0])
         self.model_dropdown.pack(fill="x")
 
-        auto_ocr_all_button = tk.Button(left_frame, text="Auto OCR All File", command=self.auto_ocr_all_click)
-        auto_ocr_all_button.pack(pady=10, fill="x")
+        self.auto_ocr_all_button = tk.Button(left_frame, text="Auto OCR All File", command=self.auto_ocr_all_click)
+        self.auto_ocr_all_button.pack(pady=10, fill="x")
 
         tk.Frame(left_frame).pack(fill="x", pady=15)
+
+        server_settings_button = tk.Button(left_frame, text="Server setting", command=self.open_server_settings_click)
+        server_settings_button.pack(pady=5, fill="x")
 
         help_button = tk.Button(left_frame, text="Help", command=self.help_click)
         help_button.pack(pady=5, fill="x")
@@ -172,6 +285,41 @@ class OCRLabelingTool(tk.Tk):
             print('cancel')
             self.after_cancel(self.resize_after_id)
         self.resize_after_id = self.after(50, self.perform_resize)
+
+    def open_server_settings_click(self):
+        is_connect_to_server_old_value = self.is_connect_to_server
+        is_open_server_old_value = self.is_open_server
+
+        settings_window = SettingsDialog(self, self.is_open_server, self.is_connect_to_server, self.port, self.base_url)
+        self.wait_window(settings_window)
+
+        self.port = settings_window.port
+        self.is_connect_to_server = settings_window.is_connect_to_server
+        self.is_open_server = settings_window.is_open_server
+        self.base_url = settings_window.base_url
+
+        self.apply_server_settings(is_connect_to_server_old_value, is_open_server_old_value)
+
+    def apply_server_settings(self, is_connect_to_server_old_value, is_open_server_old_value):
+        if self.is_open_server:
+            if is_connect_to_server_old_value:
+                self.add_log("Disconnect from another server")
+            self.add_log(f"Open server at port {self.port}")
+            # enable button
+            self.open_image_folder_button.config(state=tk.NORMAL)
+            self.open_label_folder_button.config(state=tk.NORMAL)
+            self.open_recycle_bin_folder_button.config(state=tk.NORMAL)
+            self.auto_ocr_all_button.config(state=tk.NORMAL)
+
+        elif self.is_connect_to_server:
+            if is_open_server_old_value:
+                self.add_log("Close server")
+            self.add_log(f"Connect to server at {self.base_url}")
+            # disable button
+            self.open_image_folder_button.config(state=tk.DISABLED)
+            self.open_label_folder_button.config(state=tk.DISABLED)
+            self.open_recycle_bin_folder_button.config(state=tk.DISABLED)
+            self.auto_ocr_all_button.config(state=tk.DISABLED)
 
     def perform_resize(self):
         print('perform_resize')
